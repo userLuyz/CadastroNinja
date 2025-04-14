b698ef2ab7764215793f787a5f39a555885ca19f
user <luiz.gus.ms@gmail.com>
2025-04-14 20:18:20 -0300

    first commit

---
+++ src/main/java/dev/java10x/CadastroNinja/CadastroNinjasApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroNinja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroNinjasApplication.class, args);
+	}
+
+}
