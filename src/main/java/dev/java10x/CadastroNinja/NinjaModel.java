dceea572c06253eef5ebf9e1ab5f010990c0c84a
user <luiz.gus.ms@gmail.com>
2025-04-15 18:59:59 -0300

    Criação do modelo DB

--- src/main/java/dev/java10x/CadastroNinja/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroNinja/NinjaModel.java
@@ -1,4 +1,48 @@
 package dev.java10x.CadastroNinja;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro_ninja")
 public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ID;
+    private String name;
+    private String email;
+    private short age;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String name, String email, short age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public short getAge() {
+        return age;
+    }
+
+    public void setAge(short age) {
+        this.age = age;
+    }
 }
